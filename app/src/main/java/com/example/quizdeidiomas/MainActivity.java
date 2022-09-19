7b0a4e2f00bae627fcc024f73edc92e6598bad36
2dam3 <2dam3@HZ114485.H012108.NET>
2022-09-19 12:47:45 +0200

    first commit

---
+++ app/src/main/java/com/example/quizdeidiomas/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.quizdeidiomas;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+//        getResources().getStringArray(R.array.preguntas);
+
+    }
+}
