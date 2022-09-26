a6d0b25b1a57e4ee10185fdbf992db251b766229
2dam3 <2dam3@HZ114485.H012108.NET>
2022-09-26 10:03:20 +0200

    Colores introducidos

--- app/src/main/java/com/example/quizdeidiomas/MainActivity.java
+++ app/src/main/java/com/example/quizdeidiomas/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.quizdeidiomas;
 
+import androidx.annotation.ColorInt;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -11,35 +14,114 @@ import android.widget.RadioGroup;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.Objects;
+
 public class MainActivity extends AppCompatActivity {
 
     private int botonesRespuestas[] = {
             R.id.opcion1,R.id.opcion2,R.id.opcion3,R.id.opcion4
     };
-    Button botonComprobar1,botonComprobar2,botonComprobar3,botonComprobar4;
+    private int pregunta = 0;
+    private String correcto, textoBoton;
+    Button botonComprobar1,botonComprobar2,botonComprobar3,botonComprobar4, botonContinuar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 //        getResources().getStringArray(R.array.preguntas);
-        String correcto = mostrarPregunta(0);
+
+        correcto = mostrarPregunta(pregunta);
+
+        botonContinuar = findViewById(R.id.botonContinuar);
 
         botonComprobar1 = findViewById(R.id.opcion1);
         botonComprobar2 = findViewById(R.id.opcion2);
         botonComprobar3 = findViewById(R.id.opcion3);
         botonComprobar4 = findViewById(R.id.opcion4);
 
+
+        botonContinuar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (pregunta != getResources().getStringArray(R.array.preguntas).length-1){
+                    pregunta++;
+                    correcto = mostrarPregunta(pregunta);
+                    botonComprobar1.setBackgroundColor(Color.parseColor("#FF6200EE"));
+                    botonComprobar2.setBackgroundColor(Color.parseColor("#FF6200EE"));
+                    botonComprobar3.setBackgroundColor(Color.parseColor("#FF6200EE"));
+                    botonComprobar4.setBackgroundColor(Color.parseColor("#FF6200EE"));
+                    botonComprobar1.setEnabled(true);
+                    botonComprobar2.setEnabled(true);
+                    botonComprobar3.setEnabled(true);
+                    botonComprobar4.setEnabled(true);
+                }
+            }
+        });
+
         botonComprobar1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String textoBoton = (String) botonComprobar1.getText();
-                if (correcto == textoBoton){
+                textoBoton = (String) botonComprobar1.getText();
+                if (Objects.equals(correcto, textoBoton)){
                     Toast.makeText(MainActivity.this, "Correcto", Toast.LENGTH_LONG).show();
+                    botonComprobar1.setBackgroundColor(Color.parseColor("#31C000"));
                 }else {
                     Toast.makeText(MainActivity.this, "Incorrecto", Toast.LENGTH_LONG).show();
+                    botonComprobar1.setBackgroundColor(Color.parseColor("#CD0000"));
                 }
+                botonComprobar2.setEnabled(false);
+                botonComprobar3.setEnabled(false);
+                botonComprobar4.setEnabled(false);
                 Log.i("Correcto",correcto);
                 Log.i("Texto",textoBoton);
+            }
+        });
+        botonComprobar2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                textoBoton = (String) botonComprobar2.getText();
+                if (Objects.equals(correcto, textoBoton)){
+                    Toast.makeText(MainActivity.this, "Correcto", Toast.LENGTH_LONG).show();
+                    botonComprobar2.setBackgroundColor(Color.parseColor("#31C000"));
+                }else {
+                    Toast.makeText(MainActivity.this, "Incorrecto", Toast.LENGTH_LONG).show();
+                    botonComprobar2.setBackgroundColor(Color.parseColor("#CD0000"));
+                }
+                botonComprobar1.setEnabled(false);
+                botonComprobar3.setEnabled(false);
+                botonComprobar4.setEnabled(false);
+            }
+        });
+        botonComprobar3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                textoBoton = (String) botonComprobar3.getText();
+                if (Objects.equals(correcto, textoBoton)){
+                    Toast.makeText(MainActivity.this, "Correcto", Toast.LENGTH_LONG).show();
+                    botonComprobar3.setBackgroundColor(Color.parseColor("#31C000"));
+                }else {
+                    Toast.makeText(MainActivity.this, "Incorrecto", Toast.LENGTH_LONG).show();
+                    botonComprobar3.setBackgroundColor(Color.parseColor("#CD0000"));
+                }
+                botonComprobar1.setEnabled(false);
+                botonComprobar2.setEnabled(false);
+                botonComprobar4.setEnabled(false);
+            }
+        });
+        botonComprobar4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                textoBoton = (String) botonComprobar4.getText();
+                if (Objects.equals(correcto, textoBoton)){
+                    Toast.makeText(MainActivity.this, "Correcto", Toast.LENGTH_LONG).show();
+                    botonComprobar4.setBackgroundColor(Color.parseColor("#31C000"));
+                }else {
+                    Toast.makeText(MainActivity.this, "Incorrecto", Toast.LENGTH_LONG).show();
+                    botonComprobar4.setBackgroundColor(Color.parseColor("#CD0000"));
+                }
+                botonComprobar1.setEnabled(false);
+                botonComprobar2.setEnabled(false);
+                botonComprobar3.setEnabled(false);
             }
         });
     }
