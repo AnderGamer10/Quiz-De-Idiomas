30a7d42495db54445c11905e22c6770c1002728a
AnderGamer10 <anderedondo10@hotmail.com>
2022-09-29 16:31:51 +0200

    Falta hacer la parte del mainActivity2

--- app/src/main/java/com/example/quizdeidiomas/MainActivity.java
+++ app/src/main/java/com/example/quizdeidiomas/MainActivity.java
@@ -8,6 +8,7 @@ import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.speech.tts.TextToSpeech;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -17,6 +18,7 @@ import android.widget.RadioGroup;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.Locale;
 import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
@@ -26,12 +28,14 @@ public class MainActivity extends AppCompatActivity {
     };
     private int red = Color.argb(100, 255,0,0);
     private int green = Color.argb(100, 0,255,0);
-    private int pregunta = 0, respCorrectas = 0, respIncorrectas = 0;
+    private int pregunta = 0, respCorrectas = 0, cantPreguntas = 0;
 
     private String correcto, textoBoton;
     Button botonComprobar1,botonComprobar2,botonComprobar3,botonComprobar4, botonContinuar, botonCompletar, botonReiniciar;
     ImageButton goblin, lich, sylvanas, anderwow;
-    TextView correctas, incorrectas, preguntaDosPuntos, preguntaTexto, resultados;
+    TextView preguntaDosPuntos, preguntaTexto, resultadosTexto, cantPreguntasTexto, cantAcertadasTexto, barra;
+    TextToSpeech textToSpeech;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -61,13 +65,38 @@ public class MainActivity extends AppCompatActivity {
         sylvanas.setVisibility(View.INVISIBLE);
         anderwow.setVisibility(View.INVISIBLE);
 
-        correctas = findViewById(R.id.Correctas);
-        incorrectas = findViewById(R.id.Incorrectas);
-        correctas.setVisibility(View.INVISIBLE);
-        incorrectas.setVisibility(View.INVISIBLE);
-        resultados = findViewById(R.id.Resultados);
-        resultados.setVisibility(View.INVISIBLE);
+        resultadosTexto = findViewById(R.id.Resultados);
+        resultadosTexto.setVisibility(View.INVISIBLE);
+
+        cantPreguntasTexto = findViewById(R.id.cantidadPreguntas);
+        cantAcertadasTexto = findViewById(R.id.cantidadAcertadas);
+        barra = findViewById(R.id.barra);
+        cantPreguntasTexto.setVisibility(View.INVISIBLE);
+        cantAcertadasTexto.setVisibility(View.INVISIBLE);
+        barra.setVisibility(View.INVISIBLE);
+
+//        Esto llamara por primera vez al metodo en el cual apareceran las preguntas con botones
         correcto = mostrarPregunta(pregunta);
+
+//        Text To Spech para decirnos la pregunta cuando cliquemos en ella
+        textToSpeech = new TextToSpeech(getApplicationContext(), new TextToSpeech.OnInitListener() {
+            @Override
+            public void onInit(int i) {
+
+                if(i!=TextToSpeech.ERROR){
+                    textToSpeech.setLanguage(Locale.UK);
+                }
+            }
+        });
+
+        TextView textPreguntas = (TextView) findViewById(R.id.preguntaId);
+
+        textPreguntas.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                textToSpeech.speak(textPreguntas.getText().toString(),TextToSpeech.QUEUE_FLUSH,null);
+            }
+        });
 
         botonContinuar.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -106,26 +135,32 @@ public class MainActivity extends AppCompatActivity {
                     TextView text_pregunta = (TextView) findViewById(R.id.preguntaId);
                     text_pregunta.setText(preguntas[0]);
                 }
+                cantPreguntas++;
             }
         });
 
         botonCompletar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                cantPreguntas++;
                 goblin.setVisibility(View.INVISIBLE);
                 lich.setVisibility(View.INVISIBLE);
                 sylvanas.setVisibility(View.INVISIBLE);
                 anderwow.setVisibility(View.INVISIBLE);
 
-                correctas.setVisibility(View.VISIBLE);
-                incorrectas.setVisibility(View.VISIBLE);
-                resultados.setVisibility(View.VISIBLE);
+                resultadosTexto.setVisibility(View.VISIBLE);
 
                 preguntaDosPuntos.setVisibility(View.INVISIBLE);
                 preguntaTexto.setVisibility(View.INVISIBLE);
+                botonCompletar.setVisibility(View.INVISIBLE);
+
+                cantPreguntasTexto.setVisibility(View.VISIBLE);
+                cantAcertadasTexto.setVisibility(View.VISIBLE);
+                barra.setVisibility(View.VISIBLE);
+                cantPreguntasTexto.setText(String.valueOf(cantPreguntas));
+                cantAcertadasTexto.setText(String.valueOf(respCorrectas));
 
                 Log.i("correctas", String.valueOf(respCorrectas));
-                Log.i("incorrectas", String.valueOf(respIncorrectas));
             }
         });
 
@@ -140,9 +175,8 @@ public class MainActivity extends AppCompatActivity {
         goblin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(MainActivity.this, "Incorrecto", Toast.LENGTH_LONG).show();
+                Toast.makeText(MainActivity.this, "Incorrecto", Toast.LENGTH_SHORT).show();
                 goblin.setColorFilter(red);
-                respIncorrectas++;
                 goblin.setClickable(false);
                 lich.setEnabled(false);
                 sylvanas.setEnabled(false);
@@ -152,7 +186,7 @@ public class MainActivity extends AppCompatActivity {
         lich.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(MainActivity.this, "Correcto", Toast.LENGTH_LONG).show();
+                Toast.makeText(MainActivity.this, "Correcto", Toast.LENGTH_SHORT).show();
                 lich.setColorFilter(green);
                 respCorrectas++;
                 goblin.setEnabled(false);
@@ -164,9 +198,8 @@ public class MainActivity extends AppCompatActivity {
         sylvanas.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(MainActivity.this, "Incorrecto", Toast.LENGTH_LONG).show();
+                Toast.makeText(MainActivity.this, "Incorrecto", Toast.LENGTH_SHORT).show();
                 sylvanas.setColorFilter(red);
-                respIncorrectas++;
                 goblin.setEnabled(false);
                 lich.setEnabled(false);
                 sylvanas.setClickable(false);
@@ -176,9 +209,8 @@ public class MainActivity extends AppCompatActivity {
         anderwow.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(MainActivity.this, "Incorrecto", Toast.LENGTH_LONG).show();
+                Toast.makeText(MainActivity.this, "Incorrecto", Toast.LENGTH_SHORT).show();
                 anderwow.setColorFilter(red);
-                respIncorrectas++;
                 goblin.setEnabled(false);
                 lich.setEnabled(false);
                 sylvanas.setEnabled(false);
@@ -192,13 +224,12 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 textoBoton = (String) botonComprobar1.getText();
                 if (Objects.equals(correcto, textoBoton)){
-                    Toast.makeText(MainActivity.this, "Correcto", Toast.LENGTH_LONG).show();
+                    Toast.makeText(MainActivity.this, "Correcto", Toast.LENGTH_SHORT).show();
                     botonComprobar1.setBackgroundColor(Color.parseColor("#31C000"));
                     respCorrectas++;
                 }else {
-                    Toast.makeText(MainActivity.this, "Incorrecto", Toast.LENGTH_LONG).show();
+                    Toast.makeText(MainActivity.this, "Incorrecto", Toast.LENGTH_SHORT).show();
                     botonComprobar1.setBackgroundColor(Color.parseColor("#CD0000"));
-                    respIncorrectas++;
                 }
                 botonComprobar1.setClickable(false);
                 botonComprobar2.setEnabled(false);
@@ -211,13 +242,12 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 textoBoton = (String) botonComprobar2.getText();
                 if (Objects.equals(correcto, textoBoton)){
-                    Toast.makeText(MainActivity.this, "Correcto", Toast.LENGTH_LONG).show();
+                    Toast.makeText(MainActivity.this, "Correcto", Toast.LENGTH_SHORT).show();
                     botonComprobar2.setBackgroundColor(Color.parseColor("#31C000"));
                     respCorrectas++;
                 }else {
-                    Toast.makeText(MainActivity.this, "Incorrecto", Toast.LENGTH_LONG).show();
+                    Toast.makeText(MainActivity.this, "Incorrecto", Toast.LENGTH_SHORT).show();
                     botonComprobar2.setBackgroundColor(Color.parseColor("#CD0000"));
-                    respIncorrectas++;
                 }
                 botonComprobar2.setClickable(false);
                 botonComprobar1.setEnabled(false);
@@ -230,13 +260,12 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 textoBoton = (String) botonComprobar3.getText();
                 if (Objects.equals(correcto, textoBoton)){
-                    Toast.makeText(MainActivity.this, "Correcto", Toast.LENGTH_LONG).show();
+                    Toast.makeText(MainActivity.this, "Correcto", Toast.LENGTH_SHORT).show();
                     botonComprobar3.setBackgroundColor(Color.parseColor("#31C000"));
                     respCorrectas++;
                 }else {
-                    Toast.makeText(MainActivity.this, "Incorrecto", Toast.LENGTH_LONG).show();
+                    Toast.makeText(MainActivity.this, "Incorrecto", Toast.LENGTH_SHORT).show();
                     botonComprobar3.setBackgroundColor(Color.parseColor("#CD0000"));
-                    respIncorrectas++;
                 }
                 botonComprobar3.setClickable(false);
                 botonComprobar1.setEnabled(false);
@@ -249,13 +278,12 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 textoBoton = (String) botonComprobar4.getText();
                 if (Objects.equals(correcto, textoBoton)){
-                    Toast.makeText(MainActivity.this, "Correcto", Toast.LENGTH_LONG).show();
+                    Toast.makeText(MainActivity.this, "Correcto", Toast.LENGTH_SHORT).show();
                     botonComprobar4.setBackgroundColor(Color.parseColor("#31C000"));
                     respCorrectas++;
                 }else {
-                    Toast.makeText(MainActivity.this, "Incorrecto", Toast.LENGTH_LONG).show();
+                    Toast.makeText(MainActivity.this, "Incorrecto", Toast.LENGTH_SHORT).show();
                     botonComprobar4.setBackgroundColor(Color.parseColor("#CD0000"));
-                    respIncorrectas++;
                 }
                 botonComprobar4.setClickable(false);
                 botonComprobar1.setEnabled(false);
